41f665a653dc3d1b892f2fd5df4c38f9ba19132c
Percy Reyes <alfredocancer98@gmail.com>
2024-04-13 12:10:16 -0500

    "Agregando cors"

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express"
 import "./database/config.js"
 import morgan from "morgan"
+import cors from "cors"
 import {dataRoute} from "./routes/form.route.js"
 
 const app = express()
-
+app.use(cors())
 app.use(morgan("dev"))
 app.use(express.json())
 
